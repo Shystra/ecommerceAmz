f0d022fdcbe9f4789ed00b2eb5d3bcfef245992b
Shystra <jlsp.2019@hotmail.com>
2024-02-28 19:45:16 -0300

    feat: just firts commit dependeces

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        TS
+      </header>
+      <main>
+
+      </main>
+
+      <footer>
+        All right reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
